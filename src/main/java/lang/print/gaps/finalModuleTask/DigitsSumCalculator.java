344de18fdc5b5f91b8280089c297afaaae1812ec
Evgeniy Makeev <evgeniyvmakeev@gmail.com>
2024-06-01 23:32:10 +0300

    solution

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int thousands = number / 1000;
+        int hundreds = (number / 100) % 10;
+        int tens = (number / 10) % 10;
+        int units = number % 10;
+
+        int result = thousands + hundreds + tens + units;
+
+        System.out.println(result);
     }
 }
